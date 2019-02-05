bdcaf4b89f95f78f14bafb3345b387ce0381c2ae
David Boivin <d.boivin.0396@gmail.com>
2019-02-05 12:39:24 -0500

    Added mock method to change board
    -idea for new_game handling

--- client/Main.java
+++ client/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -6,21 +8,33 @@ import view.Style;
 
 public class Main extends Application {
 	
+	private Scene mScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		
 		
 		try {
+			
+			Inbox inbox = new Inbox();
+			KeyCard keycard = new KeyCard();
+			
+			//TODO generate card list
+			ArrayList<Card> cardList = new ArrayList<Card>();
+			for(int i = 0; i < 25; i++) {
+				cardList.add(new Card(i, codeword, k.getCardType(i)));
+			}
+			
 			//creates the board
-			BoardPane viewRoot = new BoardPane(); //TODO swap test constructor with actual constructor 
+			BoardPane viewRoot = new BoardPane(cardList, keycard, inbox);
 			
 			//links the board to the window
-			Scene scene = new Scene(viewRoot, Style.WINDOW_SIZE_WIDTH , Style.WINDOW_SIZE_HEIGHT);
+			mScene scene = new Scene(viewRoot, Style.WINDOW_SIZE_WIDTH , Style.WINDOW_SIZE_HEIGHT);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			
-			primaryStage.setScene(scene);
-			primaryStage.setTitle("CodeNames");
-			primaryStage.show();
+			mStage.setScene(scene);
+			mStage.setTitle("CodeNames");
+			mStage.show();
 			
 		} catch(Exception e) {
 			e.printStackTrace();
@@ -29,5 +43,9 @@ public class Main extends Application {
 	
 	public static void main(String[] args) {
 		launch(args);
+	}
+	
+	public void changeBoard(BoardPane root) {
+		mScene.setRoot(root);
 	}
 }

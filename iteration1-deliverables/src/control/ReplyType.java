7d6d2bf9beaafc682034f8353ee6287321547615
Kevin McAllister <kevinryan@videotron.ca>
2019-02-09 15:16:48 -0500

    Removed unnecessary files from client folder
    Moved final version of application to iteration1-deliverables

---
+++ iteration1-deliverables/src/control/ReplyType.java
@@ -0,0 +1,10 @@
+/**
+ * Reply enum used to define the types of replies sent from Control to View
+ * @author Kevin McAllister (40031326) - Iteration 1
+ */
+package control;
+
+public enum ReplyType {
+
+    UPDATE, END
+}

e0fbdd23486f4dce123bcfbe32468d6f52b0ecc3
Kevin McAllister <kevinryan@videotron.ca>
2019-02-10 17:29:20 -0500

    Emptied Master to prepare for next iteration (client will be merged as the baseline after iteration 1 was completed)

--- client/control/MessageType.java
+++
@@ -1,9 +0,0 @@
-/**
- * MessageType enum used to define the types of messages sent from the View to Control
- * @author Kevin McAllister (40031326) - Iteration 1
- */
-package control;
-
-public enum MessageType {
-    NEW_GAME, NEXT, UNDO, REDO, SELECT
-}
